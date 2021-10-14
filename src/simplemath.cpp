2912c74a39ff49efbc3b8ed3bd7ea009a2e119ed
Dipak Bari <dipakbari4@gmail.com>
2021-10-14 19:08:15 +0530

    adding two integers

---
+++ src/simplemath.cpp
@@ -0,0 +1,27 @@
+/**
+ * @file simplemath.cpp
+ * @author Dipak Bari (dipakbari4@gmail.com)
+ * @brief A small program to demonstrate the operators in CPP
+ * @version 0.1
+ * @date 2021-10-14
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int sum, a, b;
+
+  cout << "Enter value of a: ";
+  cin >> a;
+
+  cout << "Enter value of b: ";
+  cin >> b;
+
+  cout << "The sum of a and b is " << (a + b) << endl;
+
+  return 0;
+}

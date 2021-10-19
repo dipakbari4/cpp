a48273a663af61f3b9dffd70486266a0b15333b1
Dipak Bari <dipakbari4@gmail.com>
2021-10-20 00:17:27 +0530

    Vote Eligibility | If Else

---
+++ src/if_else.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int age = 15;
+
+    if (age >= 18) {
+        cout << "You're eligible for vote";
+    } else {
+        cout << "You aren't eligible for vote";
+    }
+
+    return 0;
+}

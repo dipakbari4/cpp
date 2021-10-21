168a6f79d7ebc9a933a2fa2203717ef56bc4d59c
Dipak Bari <dipakbari4@gmail.com>
2021-10-21 22:14:01 +0530

    variable assignment

---
+++ src/variable_assignment.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a, b;
+
+    a = 19; 
+    b = 33;
+
+    int c, d, e;
+
+    c = a;
+    d = b;
+    e = c + d;
+
+    cout << "a: " << a << endl;
+    cout << "b: " << b << endl;
+
+    cout << "c: " << c << endl;
+    cout << "d: " << d << endl;
+    cout << "e: " << e << endl;
+
+    return 0;
+}
